aa822cea6bf85aa365ab2aefd77f73a5766c3adf
Ox52 <harsh04241@gmail.com>
2025-03-22 00:52:08 +0530

    final changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Navbar } from "./components/Navbar";
 import { Home } from "./components/sections/Home";
 import { About } from "./components/sections/About";
 import { Projects } from "./components/sections/Projects";
+import { Contact } from "./components/sections/Contact";
 
 function App() {
   return <>
@@ -17,6 +18,8 @@ function App() {
   <About/>
 
   <Projects/>
+
+  <Contact/>
   </>;
 }
 
